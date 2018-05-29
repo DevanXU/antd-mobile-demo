ce6cc8c68f2b84970b9b5ad403af19179a67e109
devan.xu <devan.xu@foxmail.com>
2018-05-29 22:29:58 +0800

    init project with antd-mobile

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Button} from 'antd-mobile';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button>Hi antd-mobile</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
